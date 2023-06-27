85fb00daa1054d1d7955727bb84a744f10a2b638
ntorga <northontorga@gmail.com>
2023-06-27 15:58:22 -0300

    fix: simplify BackupKeys entity

--- src/Domain/Entity/BackupKeys.go
+++ src/Domain/Entity/BackupKeys.go
@@ -1,37 +1,28 @@
 package entity
 
 import (
-	"encoding/json"
-
 	iamValueObject "../ValueObject/Iam"
 )
 
 type BackupKeys struct {
-	accessKeyId     *iamValueObject.AccessKeyId
-	secretAccessKey *iamValueObject.SecretAccessKey
+	AccessKeyId     iamValueObject.AccessKeyId     `json:"accessKeyId"`
+	SecretAccessKey iamValueObject.SecretAccessKey `json:"secretAccessKey"`
 }
 
 func NewBackupKeys(
-	accessKeyId *iamValueObject.AccessKeyId,
-	secretAccessKey *iamValueObject.SecretAccessKey,
+	accessKeyId iamValueObject.AccessKeyId,
+	secretAccessKey iamValueObject.SecretAccessKey,
 ) *BackupKeys {
 	return &BackupKeys{
-		accessKeyId:     accessKeyId,
-		secretAccessKey: secretAccessKey,
+		AccessKeyId:     accessKeyId,
+		SecretAccessKey: secretAccessKey,
 	}
 }
 
-func (self BackupKeys) GetAccessKeyId() *iamValueObject.AccessKeyId {
-	return self.accessKeyId
+func (bk BackupKeys) GetAccessKeyId() iamValueObject.AccessKeyId {
+	return bk.AccessKeyId
 }
 
-func (self BackupKeys) GetSecretAccessKey() *iamValueObject.SecretAccessKey {
-	return self.secretAccessKey
-}
-
-func (self BackupKeys) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]string{
-		"accessKeyId":     self.accessKeyId.String(),
-		"secretAccessKey": self.secretAccessKey.String(),
-	})
+func (bk BackupKeys) GetSecretAccessKey() iamValueObject.SecretAccessKey {
+	return bk.SecretAccessKey
 }
